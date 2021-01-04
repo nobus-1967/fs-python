d92d6cce03d7035b26c98419c56b3567344b6c4b
nobus-1967 <avshcherbina@gmail.com>
2021-01-04 10:05:54 +0300

    Edit variants of project B6.10.4

---
+++ b6_10/b6_10_4_v2.py
@@ -0,0 +1,27 @@
+# Вариант с использованием магического метода __repr__ (основан на варианте 1)
+
+class Volunteer:
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+    
+    def __repr__(self):
+        return f'{self.name}, г. {self.city}'
+
+
+class StatusVolunteer(Volunteer):
+    def __init__(self, name, city, status):
+        super().__init__(name, city)
+        self.status = status
+        
+    def __repr__(self):
+        return f'{self.name}, г. {self.city}, статус "{self.status}"'
+        
+
+petr_ivanov = Volunteer('Петр Иванов', 'Санкт-Петербург')
+ivan_petrov = StatusVolunteer('Иван Петров', 'Москва', 'Наставник')
+
+guests = [petr_ivanov, ivan_petrov]
+for guest in guests:
+    print(guest)
+        
