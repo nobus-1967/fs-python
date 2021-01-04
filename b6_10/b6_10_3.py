5946d8e689eec38af8d9f0c8c560696e79707cef
nobus-1967 <avshcherbina@gmail.com>
2021-01-04 10:09:06 +0300

    Revert "Change files' attributes"
    This reverts commit d91b25a84b416cca0d093cc207018a3159132c21.

---
+++ b6_10/b6_10_3.py
@@ -0,0 +1,19 @@
+class OnlineWallet:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+        
+    def get_balance(self):
+        return self.balance       
+  
+    def set_balance(self, balance):
+        print('Введите новый баланс клиента:')
+        self.balance = balance
+
+    def display_info(self):
+        print(f'Клиент "{self.name}". Баланс: {self.balance} руб.')
+        
+Ivan = OnlineWallet('Иван Петров')
+Ivan.display_info()
+Ivan.balance = 50
+Ivan.display_info()
