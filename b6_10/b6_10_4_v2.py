c5edf77a0b29ccb2185a1825da771391a413331d
nobus-1967 <avshcherbina@gmail.com>
2021-01-04 10:45:14 +0300

    Edit code of project B6.10.4 v.2

--- b6_10/b6_10_4_v2.py
+++ b6_10/b6_10_4_v2.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-# Вариант с использованием магического метода __repr__
-=======
 # Вариант с использованием магического метода __repr__ (основан на варианте 1)
->>>>>>> origin/main
 
 class Volunteer:
     def __init__(self, name, city):
@@ -21,15 +17,10 @@ class StatusVolunteer(Volunteer):
     def __repr__(self):
         return f'{self.name}, г. {self.city}, статус "{self.status}"'
         
-
 petr_ivanov = Volunteer('Петр Иванов', 'Санкт-Петербург')
 ivan_petrov = StatusVolunteer('Иван Петров', 'Москва', 'Наставник')
 
 guests = [petr_ivanov, ivan_petrov]
-for guest in guests:
-    print(guest)
-<<<<<<< HEAD
-        
-=======
-        
->>>>>>> origin/main
+print('Список волонтеров:')
+for index, guest in enumerate(guests):
+    print(f'{index + 1}) {guest}')
