fa225b9867a3cf5ff6c46b7b67333301df7bfcfd
Anatoly Shcherbina <avshcherbina@gmail.com>
2021-02-14 16:28:47 +0300

    Add project B6.10

---
+++ b10_6/currencybot.py
@@ -0,0 +1,78 @@
+# Реализация Telegram-бота, который возвращает цену
+# на определённое количество валюты (евро, доллар или рубль).
+import telebot
+from extensions import KEYS, ExchangeCurrencies, APIException
+
+with open('token.txt', 'r') as config:
+    TOKEN = config.readline()
+  
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['hello'])
+def help(message: telebot.types.Message):
+    text=f'Приветствую, {message.chat.first_name}!\n\
+Начните общение с ботом с команды /start.'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text="""Справка доступна при вызове команд /start или /help.
+Для работы с ботом надо ввести команду:
+    <имя валюты, цену которой вы хотите узнать>
+    <имя валюты, в которой надо узнать цену первой валюты>
+    <количество первой валюты>
+Названия валют вводятся прописными буквами, а количество цифрами и
+разделяются пробелами, например:
+    доллар рубль 100
+Примеры команд для конвертации выводятся командой /examples.
+Информация о доступных валютах выводится командой /values.
+    """
+    bot.send_message(message.chat.id, text)
+ 
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    values = ''
+    for key in KEYS.keys():
+        values += f'{key}\n'
+    text = f'Доступные валюты:\n{values}\n\
+(доллар=доллар США,\n\
+вона=южнокорейская вона,\n\
+фунт=британский фунт,\n\
+франк=швейцарский франк,\n\
+крона=чешская крона,\n\
+рупия=индийская рупия,\n\
+лира=турецкая лира,\n\
+рэнд=южноафриканский рэнд)'
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['examples'])
+def examples(message: telebot.types.Message):
+    text ="""Примеры доступных команд для конвертации валют:
+    (100 долларов в рубли) -> доллар рубль 100
+    (50 иен в юани) -> иена юань 50
+    (200 евро в фунты) -> евро фунт 200
+Информация о доступных валютах выводится командой /values.
+При конвертации итоговая сумма округляется до целого числа!
+    """
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise APIException('Обратитесь к командам /help или /example.')       
+    except APIException as e:
+        bot.send_message(message.chat.id,
+                         f'Ошибка ввода команды!\n{e}')
+    except Exception as e:
+        bot.send_message(message.chat.id,
+                         'Не удалось обработать команду {e}!')
+    else:
+        base, quote, amount = values
+        total = ExchangeCurrencies.get_price(base, quote, amount)
+        bot.reply_to(message, total)
+
+
+bot.polling(none_stop=True)
---
+++ b10_6/extensions.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+KEYS = {'рубль': 'RUB',
+        'евро': 'USD',
+        'доллар': 'EUR',
+        'юань': 'CNY',
+        'иена': 'JPY',
+        'вона': 'KRW',
+        'фунт': 'GBP',
+        'франк': 'CHF',
+        'крона': 'CZK',
+        'рупия': 'INR',
+        'лира': 'TRY',
+        'шекель': 'ILS',
+        'рэнд': 'ZAR'}
+
+class APIException(Exception):
+    pass
+
+class ExchangeCurrencies:
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str) -> str:
+        try:
+            base = KEYS[base]
+            quote = KEYS[quote]
+            int_amount = int(amount)
+            if base == quote:
+                raise APIException('Нельзя конвертировать одну и ту же валюту!')
+        except APIException as e:
+            return f'{e}'
+        except KeyError:
+            return 'Такая валюта в списке доступных отсутствует!'
+        except ValueError:
+            return 'Количество первой валюты: введите целое число больше 0!'
+        else:                
+            if base == 'EUR':
+                cur_info = requests.get(f'https://api.exchangeratesapi.io/latest?symbols={quote}')
+            else:
+                cur_info = requests.get(f'https://api.exchangeratesapi.io/latest?base={base}&symbols={quote},{base}')
+                rate = json.loads(cur_info.content)['rates']
+                total = rate[quote] * int_amount
+        
+        return f'{total: .2f}' 
